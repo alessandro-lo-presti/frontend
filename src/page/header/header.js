0ad4e46a458c5659cd04a253bceb4f8519a37ef3
Alessandro Lo Presti <mitlopresti@hotmail.it>
2021-05-27 17:25:55 +0200

    change section on header link click

--- src/page/header/header.js
+++ src/page/header/header.js
@@ -1,14 +1,16 @@
+import { loadRanking } from "../ranking/ranking";
+import { loadHome } from "../home/home";
 import "./header.scss";
 
 export const initializeHeader = () => {
     document.querySelectorAll(".nav-link").forEach((item) => {
         item.addEventListener("click", (event) => {
-            console.log("evento cambio sezione", event.target);
-            //per ogni click -> renderizza la sezione giusta!
 
             //leggere data-section
+            const section = event.target.getAttribute('data-section');
 
             //caricare il loadXXX giusto loadHome loadRanking etc etc
+            section == 'HOME' ? loadHome() : loadRanking();
         });
     });
 };
