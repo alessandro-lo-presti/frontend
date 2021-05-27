b54cbffb0cd294f83592b94b6aa85315ff77eaeb
Alessandro Lo Presti <mitlopresti@hotmail.it>
2021-05-27 15:09:51 +0200

    table first look

--- src/ranking/ranking.js
+++ src/ranking/ranking.js
@@ -1,14 +1,52 @@
 import { writeMainHTML } from "../common/common";
 import { movieApiService } from "../services/movieApiService";
+import "./ranking.scss"
 
 const recordTemplate = movie => `
-    <div class"movie-table-record">
-        ${movie.name}
+    <div class="movie-table-record">
+
+        <div class="movie-id">
+           ${movie.id}
+        </div>
+
+        <div class="movie-name">
+            ${movie.name}
+        </div>
+
+        <div class="movie-rating">
+            ${movie.rating}
+        </div>
+
+        <div class="movie-views">
+            ${movie.views}
+        </div>
+    
     </div>
 `;
 
 const buildRanking = movieList => `
     <div class="movie-table">
+
+        <div class="movie-table-record">
+
+            <div class="movie-id">
+                <h2>Id</h2>
+            </div>
+
+            <div class="movie-name">
+                <h2>Nome</h2>
+            </div>
+
+            <div class="movie-rating">
+                <h2>Voto</h2>
+            </div>
+
+            <div class="movie-views">
+                <h2>Visual</h2>
+            </div>
+
+        </div>
+
         ${movieList.map(movie => recordTemplate(movie)).join("")}
     </div>
 `;
