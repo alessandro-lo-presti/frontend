83fb5f75222a32a1c270f4b16de20a599887390e
Alessandro Lo Presti <mitlopresti@hotmail.it>
2021-05-27 11:01:31 +0200

    fix container style

--- src/page/header/header.js
+++ src/page/header/header.js
@@ -3,24 +3,28 @@ import "./header.scss";
 
 const headerTemplate = `
     <header class="header">
-        
-        <div class="logo">
-            Brandname
+
+        <div class="container">
+
+            <div class="logo">
+                Brandname
+            </div>
+
+            <nav class="nav">
+                <ul class="nav-list">
+
+                    <li class="nav-link">
+                        Home
+                    </li>
+
+                    <li class="nav-link">
+                        Classifica
+                    </li>
+                    
+                </ul>
+            </nav>
+
         </div>
-
-        <nav class="nav">
-            <ul class="nav-list">
-
-                <li class="nav-link">
-                    Home
-                </li>
-
-                <li class="nav-link">
-                    Classifica
-                </li>
-                
-            </ul>
-        </nav>
 
     </header>
 `;
