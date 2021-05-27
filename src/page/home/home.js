6bbb572b9eb4c94a05d1473f4b5278ef253b82db
Alessandro Lo Presti <mitlopresti@hotmail.it>
2021-05-27 12:49:51 +0200

    timer first look

--- src/page/home/home.js
+++ src/page/home/home.js
@@ -2,7 +2,13 @@ import { movieApiService } from "../../services/movieApiService";
 import { writeMainHTML } from "../../common/common";
 import "./home.scss";
 
-const movieTemplate = (movie) => `
+const timer = (end, date) => `
+    ${Math.floor(((end - date) / 1000) / 3600 % 24)}:
+    ${Math.floor(((end - date) / 1000) / 60 % 60)}:
+    ${Math.floor(((end - date) / 1000)) % 60}
+`
+
+const movieTemplate = (movie, date) => `
     <div class="card">
         
         <div class="card-img">
@@ -11,7 +17,9 @@ const movieTemplate = (movie) => `
 
         <div class="card-infos">
             <span class="card-info">${movie.name}</span>
-            <span class="card-info countdown">${movie.end}</span>
+            <span class="card-info countdown">
+                ${timer(movie.end, date)}
+            </span>
         </div>
 
     </div>
@@ -19,12 +27,14 @@ const movieTemplate = (movie) => `
 
 const buildHome = movielist => `
     <div class="card-wrapper">
-        ${movielist.map(movie => movieTemplate(movie)).join("")}
+        ${movielist.map(movie => movieTemplate(movie, new Date())).join("")}
     </div>
 `;
 
 export const home = () => {
     movieApiService.movieList()
-        .then(movielist => writeMainHTML(buildHome(movielist)))
+        .then(movielist => {
+                writeMainHTML(buildHome(movielist))
+            })
         .catch(() => writeMainHTML("Errore ricezione dati"))
 };
--- src/services/movieApiService.js
+++ src/services/movieApiService.js
@@ -7,48 +7,48 @@ const movielist = () => {
                     name: 'Braveheart',
                     views: 1000,
                     rating: 8.5,
-                    img: '',
-                    end: 1000
+                    img: '../img/gattaca.jpg',
+                    end: new Date("06-10-2021 00:00")
                 },
                 {
                     id: 2,
                     name: 'Pulp Fiction',
                     views: 2500,
                     rating: 9.1,
-                    img: '',
-                    end: 1000
+                    img: '../img/gattaca.jpg',
+                    end: new Date("06-10-2021 00:00")
                 },
                 {
                     id: 3,
                     name: 'Goodbye Lenin',
                     views: 400,
                     rating: 8.9,
-                    img: '',
-                    end: 1000
+                    img: '../img/gattaca.jpg',
+                    end: new Date("06-10-2021 00:00")
                 },
                 {
                     id: 4,
                     name: 'Harry a Pezzi',
                     views: 1200,
                     rating: 8.8,
-                    img: '',
-                    end: 1000
+                    img: '../img/gattaca.jpg',
+                    end: new Date("06-10-2021 00:00")
                 },
                 {
                     id: 5,
                     name: 'Gattaca',
                     views: 700,
                     rating: 9.0,
-                    img: '',
-                    end: 1000
+                    img: '../img/gattaca.jpg',
+                    end: new Date("06-10-2021 00:00")
                 },
                 {
                     id: 6,
                     name: 'Hot Fuzz',
                     views: 100,
                     rating: 8.2,
-                    img: '',
-                    end: 1000
+                    img: '../img/gattaca.jpg',
+                    end: new Date("06-10-2021 00:00")
                 },
             ]
         );
