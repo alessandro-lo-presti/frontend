472d9253eba94dcd9fd09ba9c0f0ef3ddcaf12f1
Alessandro Lo Presti <mitlopresti@hotmail.it>
2021-06-03 12:40:59 +0200

    table look

--- frontend-react/src/components/ranking/Ranking.js
+++ frontend-react/src/components/ranking/Ranking.js
@@ -1,9 +1,57 @@
+import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, makeStyles, Paper, Container } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { movieApi } from "../../services/ApiServices";
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
 function Ranking() {
+    const [movies, setMovies] = useState([]);
+    const classes= useStyles();
+
+    useEffect(() => {
+
+        movieApi()
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
 
     return (
-        <div>
-            Ranking
-        </div>
+        <Container maxWidth="md">
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>ID</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Rating</TableCell>
+                            <TableCell>Views</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {movies.map((movie) => (
+                        <TableRow key={movie.name}>
+                        <TableCell component="th" scope="row">
+                            {movie.id}
+                        </TableCell>
+                        <TableCell>{movie.name}</TableCell>
+                        <TableCell>{movie.rating}</TableCell>
+                        <TableCell>{movie.views}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Container>
     );
 }
 
