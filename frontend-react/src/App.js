9ca83d845e983b5793d1128d2b4044a200fac432
Alessandro Lo Presti <mitlopresti@hotmail.it>
2021-06-03 12:17:48 +0200

    not found route

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Ranking from "./components/ranking/Ranking";
+import NotFound from "./components/error/NotFound";
 
 function App() {
     const theme = useMemo(
@@ -30,6 +31,9 @@ function App() {
                     </Route>
                     <Route path="/ranking">
                         <Ranking />
+                    </Route>
+                    <Route path="*">
+                        <NotFound />
                     </Route>
                 </Switch>
             </Router>
