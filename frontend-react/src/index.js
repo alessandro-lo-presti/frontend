88bd5cac1db4d4bfea3ff7bb232040ecb0f1fe01
Alessandro Lo Presti <mitlopresti@hotmail.it>
2021-06-07 17:32:13 +0200

    index tab

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./css/index.css";
 import store from "./redux/store";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
