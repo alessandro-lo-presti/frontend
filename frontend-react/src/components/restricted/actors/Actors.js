7c1c164176c6820403f845c6430a72d88f3bebf7
Alessandro Lo Presti <mitlopresti@hotmail.it>
2021-06-09 17:32:07 +0200

    fix promises values

--- frontend-react/src/components/restricted/actors/Actors.js
+++ frontend-react/src/components/restricted/actors/Actors.js
@@ -1,71 +1,62 @@
 import { useEffect, useState } from "react";
 import { ApiService } from "../../../services/ApiServices";
 import {
-    actorsSelector,
-    actorsSuccessAction,
-    actorsErrorAction,
+  actorsSelector,
+  actorsSuccessAction,
+  actorsErrorAction,
 } from "../../../redux/slices/actorsSlice";
 import { connect } from "react-redux";
 import { CircularProgress, Container } from "@material-ui/core";
 import ActorCard from "../../../common/ActorCard";
 
 const mapStateToProps = (state) => ({
-    actors: actorsSelector(state),
+  actors: actorsSelector(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    actorsSuccess: (actors, favourites) =>
-        dispatch(actorsSuccessAction(actors, favourites)),
-    actorsError: () => dispatch(actorsErrorAction()),
+  actorsSuccess: (actors, favourites) =>
+    dispatch(actorsSuccessAction(actors, favourites)),
+  actorsError: () => dispatch(actorsErrorAction()),
 });
 
 function Actors(props) {
-    const { actors, actorsSuccess, actorsError } = props;
-    const [loading, setLoading] = useState(false);
+  const { actors, actorsSuccess, actorsError } = props;
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setLoading(true);
+  useEffect(() => {
+    setLoading(true);
 
-        Promise.all([ApiService.actorApi(), ApiService.favoriteApi()]) //[2,3]
-            .then((values) => {
-                //TODO sistemare!
-                console.log(values[0], values[1]);
-                const result = {};
-                values.forEach((value) =>
-                    value.user_id
-                        ? (result.favourites = value)
-                        : (result.actors = value)
-                );
-                return result;
-            })
-            .then((data) => {
-                setLoading(false);
-                actorsSuccess(data.actors, data.favourites);
-            })
-            .catch(() => {
-                setLoading(false);
-                actorsError();
-            });
-    }, [actorsSuccess, actorsError]);
+    Promise.all([ApiService.actorApi(), ApiService.favoriteApi()])
+      .then((values) => {
+        const result = {};
+        result.actors = values[0];
+        result.favourites = values[1];
+        return result;
+      })
+      .then((data) => {
+        setLoading(false);
+        actorsSuccess(data.actors, data.favourites);
+      })
+      .catch(() => {
+        setLoading(false);
+        actorsError();
+      });
+  }, [actorsSuccess, actorsError]);
 
-    //TODO gestire caso errore chaimata api
-    return (
-        <Container maxWidth="md">
-            <div className="grid-container">
-                {!loading ? (
-                    actors.map((actor) => (
-                        <ActorCard
-                            name={actor.name}
-                            actor={actor.id}
-                            key={actor.id}
-                        />
-                    ))
-                ) : (
-                    <CircularProgress />
-                )}
-            </div>
-        </Container>
-    );
+  //TODO gestire caso errore chaimata api
+  return (
+    <Container maxWidth="md">
+      <div className="grid-container">
+        {!loading ? (
+          actors.map((actor) => (
+            <ActorCard name={actor.name} actor={actor.id} key={actor.id} />
+          ))
+        ) : (
+          <CircularProgress />
+        )}
+      </div>
+    </Container>
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Actors);
