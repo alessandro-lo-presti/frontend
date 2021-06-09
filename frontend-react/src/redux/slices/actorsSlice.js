f9f54fb22c016fc838b2c2a9c76d2d6c049faf3a
Alessandro Lo Presti <mitlopresti@hotmail.it>
2021-06-09 11:43:00 +0200

    actorsSlice

--- frontend-react/src/redux/slices/actorsSlice.js
+++ frontend-react/src/redux/slices/actorsSlice.js
@@ -1,11 +1,41 @@
 const initialState = {
   actors: [],
-  favorites: [],
+  favourites: [],
 };
 
 // selectors
 const actorsSelector = (state) => state.actorsSlice.actors;
-const favoritesSelector = (state) => state.favoritesSelector;
+const favouritesSelector = (state) => state.favouritesSelector;
+
+// actions
+const ACTORS_SUCCESS = "ACTORS_SUCCESS";
+const ACTORS_ERROR = "ACTORS_ERROR";
+
+const actorsSuccessAction = (actors, favourites) => ({
+  type: ACTORS_SUCCESS,
+  actors: actors,
+  favourites: favourites,
+});
+
+const actorsErrorAction = () => ({
+  type: ACTORS_ERROR,
+});
 
 // reducers
-export const actorsReducer = () => {};
+export const actorsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTORS_SUCCESS: {
+      return {
+        ...initialState,
+        actors: action.actors,
+        favourites: action.favourites,
+      };
+    }
+    case ACTORS_ERROR: {
+      return { ...initialState, actors: [], favourites: [] };
+    }
+    default: {
+      return state;
+    }
+  }
+};
