982f1fe55662d70382432a7fe781e0b0207c33ed
Davide Macocchi <davide.macocchi@alten.it>
2021-06-11 09:49:36 +0200

    fix getConnection

--- backend/src/db/dbService.js
+++ backend/src/db/dbService.js
@@ -1,166 +1,174 @@
 import mysql from "mysql";
 
-var db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "movieapp",
-});
+let db = null;
+const getDbConnection = () => {
+    return db && db.state === "connected"
+        ? db
+        : mysql.createConnection({
+              host: "localhost",
+              user: "root",
+              password: "",
+              database: "movieapp",
+          });
+};
 
 // users
 const findUser = (username, password) => {
-  return new Promise((resolve, reject) => {
-    db.query(
-      `SELECT * FROM user WHERE username = "${username}"`,
-      (error, result) => {
-        if (error) {
-          return reject(500);
-        }
-        if (result.length === 0 || result[0].password != password) {
-          return reject(400);
-        }
-        return resolve(result[0].id);
-      }
-    );
-  });
+    return new Promise((resolve, reject) => {
+        getDbConnection().query(
+            `SELECT * FROM user WHERE username = "${username}"`,
+            (error, result) => {
+                if (error) {
+                    return reject(500);
+                }
+                if (result.length === 0 || result[0].password != password) {
+                    return reject(400);
+                }
+                return resolve(result[0].id);
+            }
+        );
+    });
 };
 
 // movies
 const getMovies = () => {
-  return new Promise((resolve, reject) => {
-    db.query("SELECT * FROM movie", (error, results) => {
-      if (error) {
-        return reject(500);
-      }
-      return resolve(results);
+    return new Promise((resolve, reject) => {
+        getDbConnection().query("SELECT * FROM movie", (error, results) => {
+            if (error) {
+                return reject(500);
+            }
+            return resolve(results);
+        });
     });
-  });
 };
 
 // ranking
 const getRanking = () => {
-  return new Promise((resolve, reject) => {
-    db.query(
-      "SELECT * FROM movie JOIN ranking ON movie.id = ranking.id",
-      (error, results) => {
-        if (error) {
-          return reject(500);
-        }
-        return resolve(results);
-      }
-    );
-  });
+    return new Promise((resolve, reject) => {
+        getDbConnection().query(
+            "SELECT * FROM movie JOIN ranking ON movie.id = ranking.id",
+            (error, results) => {
+                if (error) {
+                    return reject(500);
+                }
+                return resolve(results);
+            }
+        );
+    });
 };
 
 // actors
 const getActors = () => {
-  return new Promise((resolve, reject) => {
-    db.query("SELECT * FROM actor", (error, results) => {
-      if (error) {
-        return reject(500);
-      }
-      return resolve(results);
+    return new Promise((resolve, reject) => {
+        getDbConnection().query("SELECT * FROM actor", (error, results) => {
+            if (error) {
+                return reject(500);
+            }
+            return resolve(results);
+        });
     });
-  });
 };
 
 // favorites
 const getFavouritesByUser = (userId) => {
-  return new Promise((resolve, reject) => {
-    db.query(
-      `SELECT * FROM users_actors WHERE user_id = "${userId}"`,
-      (error, results) => {
-        if (error) {
-          return reject(500);
-        }
-        if (results.length === 0) {
-          return reject(400);
-        }
-        return resolve(results.map((row) => row.actor_id));
-      }
-    );
-  });
+    return new Promise((resolve, reject) => {
+        getDbConnection().query(
+            `SELECT * FROM users_actors WHERE user_id = "${userId}"`,
+            (error, results) => {
+                if (error) {
+                    return reject(500);
+                }
+                if (results.length === 0) {
+                    return reject(400);
+                }
+                return resolve(results.map((row) => row.actor_id));
+            }
+        );
+    });
 };
 
 const toggleFavouriteActorCheck = (userId, actorId) => {
-  const userFavourite = new Promise((resolve, reject) => {
-    db.query(
-      `SELECT * FROM users_actors WHERE user_id = "${userId}"`,
-      (error, results) => {
-        if (error) {
-          return reject(500);
-        }
-        return resolve(results.map((row) => row.actor_id));
-      }
-    );
-  });
-
-  const userExist = new Promise((resolve, reject) => {
-    db.query(`SELECT * FROM user WHERE id = "${userId}"`, (error, results) => {
-      if (error) {
-        return reject(500);
-      }
-      if (results.length === 0) {
-        return reject(400);
-      }
-      return resolve();
+    const userFavourite = new Promise((resolve, reject) => {
+        getDbConnection().query(
+            `SELECT * FROM users_actors WHERE user_id = "${userId}"`,
+            (error, results) => {
+                if (error) {
+                    return reject(500);
+                }
+                return resolve(results.map((row) => row.actor_id));
+            }
+        );
     });
-  });
 
-  const actorExist = new Promise((resolve, reject) => {
-    db.query(
-      `SELECT * FROM actor WHERE id = "${actorId}"`,
-      (error, results) => {
-        if (error) {
-          return reject(500);
-        }
-        if (results.length === 0) {
-          return reject(400);
-        }
-        return resolve();
-      }
-    );
-  });
+    const userExist = new Promise((resolve, reject) => {
+        getDbConnection().query(
+            `SELECT * FROM user WHERE id = "${userId}"`,
+            (error, results) => {
+                if (error) {
+                    return reject(500);
+                }
+                if (results.length === 0) {
+                    return reject(400);
+                }
+                return resolve();
+            }
+        );
+    });
 
-  return [userFavourite, userExist, actorExist];
+    const actorExist = new Promise((resolve, reject) => {
+        getDbConnection().query(
+            `SELECT * FROM actor WHERE id = "${actorId}"`,
+            (error, results) => {
+                if (error) {
+                    return reject(500);
+                }
+                if (results.length === 0) {
+                    return reject(400);
+                }
+                return resolve();
+            }
+        );
+    });
+
+    return [userFavourite, userExist, actorExist];
 };
 
 const toggleFavouriteActor = (favourites, userId, actorId) => {
-  if (!favourites.includes(actorId)) {
-    return new Promise((resolve, reject) => {
-      db.query(
-        `INSERT INTO users_actors (user_id, actor_id) VALUES ('${userId}', '${actorId}')`,
-        (error) => {
-          if (error) {
-            return reject(500);
-          }
-          favourites.push(actorId);
-          return resolve(favourites);
-        }
-      );
-    });
-  } else {
-    return new Promise((resolve, reject) => {
-      db.query(
-        `DELETE FROM users_actors WHERE user_id = "${userId}" AND actor_id = "${actorId}" `,
-        (error) => {
-          if (error) {
-            return reject(500);
-          }
-          favourites.splice(favourites.indexOf(actorId), 1);
-          return resolve(favourites);
-        }
-      );
-    });
-  }
+    if (!favourites.includes(actorId)) {
+        return new Promise((resolve, reject) => {
+            db.query(
+                `INSERT INTO users_actors (user_id, actor_id) VALUES ('${userId}', '${actorId}')`,
+                (error) => {
+                    if (error) {
+                        return reject(500);
+                    }
+                    favourites.push(actorId);
+                    return resolve(favourites);
+                }
+            );
+        });
+    } else {
+        return new Promise((resolve, reject) => {
+            db.query(
+                `DELETE FROM users_actors WHERE user_id = "${userId}" AND actor_id = "${actorId}" `,
+                (error) => {
+                    if (error) {
+                        return reject(500);
+                    }
+                    favourites.splice(favourites.indexOf(actorId), 1);
+                    return resolve(favourites);
+                }
+            );
+        });
+    }
 };
 
 export const DB_SERVICE = {
-  findUser: findUser,
-  getMovies: getMovies,
-  getRanking: getRanking,
-  getActors: getActors,
-  getFavouritesByUser: getFavouritesByUser,
-  toggleFavouriteActor: toggleFavouriteActor,
-  toggleFavouriteActorCheck: toggleFavouriteActorCheck,
+    findUser: findUser,
+    getMovies: getMovies,
+    getRanking: getRanking,
+    getActors: getActors,
+    getFavouritesByUser: getFavouritesByUser,
+    toggleFavouriteActor: toggleFavouriteActor,
+    toggleFavouriteActorCheck: toggleFavouriteActorCheck,
 };
